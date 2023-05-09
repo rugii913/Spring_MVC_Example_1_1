51fedb8add7132d43efe5b7b283458471aa49ede
rugii913 <rugii913@gmail.com>
2023-05-10 00:07:20 +0900

    HTTP 요청 - 요청에 대한 기본 정보, 헤더 등 조회하기
    ㅁ스프링 HTTP 요청 기본 정보 및 헤더 정보 간편하게 조회 가능함
     ㅇ애노테이션 기반 스프링 컨트롤러는 파라미터로 다양한 타입들을 받음
      * 인터페이스로 정형화된 것이 아니기 때문
     ㅇ이렇게 파라미터 타입에 해당하는 인자를 받아서 log.info(~ , ~)로
     편리하게 조회할 수 있음
    ㅁ애노테이션으로 받아오는 파라미터
     ㅇ@RequestHeader MultiValueMap<String, String> headerMap
     ㅇ@CookieValue(value = "myCookie", required = false) String cookie

---
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,40 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+// 롬복 애노테이션 아래 코드를 자동으로 생성해서 로그를 선언해줌 -> 편리하게 log 객체로 받아서 사용하면 됨
+// private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(RequestHeaderController.class);
+@RestController
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie) {
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+
+        return "ok";
+    }
+}
